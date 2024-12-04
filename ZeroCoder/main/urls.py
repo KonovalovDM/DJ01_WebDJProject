f7e2800096b604505df5f682ae714cddc887691f
KonovalovDM <kdm232006@gmail.com>
2024-12-04 22:46:59 +0300

    added code to release_dj02 branch

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-      path('', views.index),
-      path('new', views.new),
-      path('data', views.data),
-      path('test', views.test)
+      path('', views.index, name='home'),
+      path('new', views.new, name='new'),
+      path('data', views.data, name='data'),
+      path('test', views.test, name='test')
 ]
